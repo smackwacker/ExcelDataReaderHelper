0e2a240740469f097f0edd15f67b860f5d111eee
Raul Menezes <raul.fuji@gmail.com>
2015-08-18 13:58:11 -0300

    - Enables support for custom columns in Excel with mapping to class using attributes.

---
+++ Excel.Helper/ExcelColumn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel.Helper
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class ExcelColumnAttribute : Attribute
+    {
+        private readonly string _columnName;
+
+        public ExcelColumnAttribute(string columnName)
+        {
+            _columnName = columnName;
+        }
+
+        public string ColumnName
+        {
+            get { return _columnName; }
+        }
+    }
+}
